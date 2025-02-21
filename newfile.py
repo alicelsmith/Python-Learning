30bdc13eb13933486d22510d70c21e321153cc60
Alice Smith <alice.smith90@icloud.com>
2025-02-21 11:13:34 +0000

    added option

--- newfile.py
+++ newfile.py
@@ -2,8 +2,8 @@ hungry = input("Are you hungry? ")
 if hungry=="Yes":
     print("Eat samosa")
     print("Eat fries")
+    print("Eat pizza")
 else:
     thirsty = input("Are you thirsty?")
     if thirsty == "yes":
         print("drink water")
-        print("drink soda")
